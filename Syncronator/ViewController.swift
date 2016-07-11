56a49f03593dc191112878447de678a71a4f8ae8
Luiz Fonseca <eu@luiz.cc>
2016-07-11 20:16:43 -0300

    Initial Commit

---
+++ Syncronator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Syncronator
+//
+//  Created by Luiz Fonseca on 11/07/2016.
+//  Copyright © 2016 Luiz Fonseca. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
